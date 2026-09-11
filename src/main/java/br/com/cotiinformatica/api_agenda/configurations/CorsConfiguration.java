5c5411e21c7a78bb0f08961c025e3993d26af6fe
gabriel22091998 <gabrielpereira1101@gmail.com>
2026-09-11 12:53:55 -0300

    feat:adiciona consulta , exclução e edição de tarefas e altera a configurção dos cors

--- src/main/java/br/com/cotiinformatica/api_agenda/configurations/CorsConfiguration.java
+++ src/main/java/br/com/cotiinformatica/api_agenda/configurations/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.cotiinformatica.api_agenda.configurations;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,12 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
 
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigin = "http://localhost:4200";
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
         registry
                 .addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(allowedOrigin)
                 .allowedMethods("POST", "PUT", "DELETE", "GET")
                 .allowedHeaders("*");
     }
